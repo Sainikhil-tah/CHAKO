6af891b2dbfc8a77fbbfd9d17c314a361f3a8464
Sainikhil-tah <sainikhilchakravarthi@gmail.com>
2026-05-19 23:44:52 +0530

    Refactor Move class and add MoveGenerator class

--- include/Move.h
+++ include/Move.h
@@ -1,37 +1,35 @@
-// to prevent redefining 
 #pragma once
-#include<string>
-#include "Constants.h"
-
-// we will define a class to store all the data a move need to carry 
-class Move{
-    // make it public as we need to access it from main.cpp file 
+#include"Board.h"
+#include"Move.h"
+#include<vector>
+class MoveGenerator{
+    // no need of constructor as nothing to initialise 
     public:
-        //the row and col we are at 
-        int fromRow,fromCol;
-        //the row and col we are moving to
-        int toRow,toCol;
-        //the piece we are moving
-        char piece;
-        //the piece we captured if captured any otherwise NULL
-        char captured;
-        //if the piece was promoted (this is important because in the undo,redo move we should know from what it got promoted and to which promotion it took)
-        char promotion;
-        //did this piece under go enpassant
-        bool isEnPassant;
-        //did this piece under go castling
-        bool isCastle;
-        // these last three are essential for undo and redo of moves 
-        //constructor 
-        Move();
-        //constructor
-        Move(int fr,int fc,int tr,int tc,char piece=EMPTY,char captured=EMPTY,char promo=EMPTY,bool ep=false,bool castle=false);
-        // we need a function which converts the move into the valid chess notation like e2e3 e7e8q  this is important so we can get stockfish-uci responses
-        // make the function const as it should not change any data 
-        std::string toAlgebraic() const;
-        // this is required it makes checking easy 
-        // Move a==Move b simply b is a value in vector of legal moves
-        // we check if the current move is there in legal Move
-        // operator overloading 
-        bool operator==(const Move& other) const;
+        // vector to store legal moves
+        std::vector<Move> generateLegalMoves(Board& board) const;
+        bool hasLegalMove(Board& board) const;
+        std::vector<Move> generateCaptureMoves(const Board& board) const;
+    private:
+        // generate all moves doesnot care if we are in check or not to make it into legal move we must make sure that after the move the king is not in check 
+        std::vector<Move> generatePseudoLegal(const Board& board) const;
+        void generatePawnMoves(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateKnightMoves(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateBishopMoves(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateRookMoves(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateQueenMoves(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateKingMoves(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        // pieces like rook,queen,bishop.. move in a direction a specific direction until its valid so we use this sliding to see those moves
+        void generateSliding(const Board& b, int r, int c, int dr, int dc, std::vector<Move>& moves) const;
+        void addPromotions(const Board& b, int fr, int fc, int tr, int tc, char captured, std::vector<Move>& moves) const;
+        // this can be moved into utlis.h 
+        // static as it doesnot depend on object 
+        static bool inBounds(int r, int c);
+        // these are important for quiesence search after reaching a depth==0 to avoid horizon effect we use this to see any violent effect of moves are possible for any pieces like capturing high level piece or promotion
+        void generatePawnCaptures(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateKnightCaptures(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateBishopCaptures(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateRookCaptures(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateQueenCaptures(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateKingCaptures(const Board& b, int r, int c, std::vector<Move>& moves) const;
+        void generateSlidingCaptures(const Board& b, int r, int c, int dr, int dc, std::vector<Move>& moves) const;
 };
